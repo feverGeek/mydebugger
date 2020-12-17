5c4d2e53c7f80a621ed154be32866a98e13dc4b8
feverGeek <1265179960@qq.com>
2020-12-17 15:39:52 +0800

    add file_fuzzer.py

--- my_test.py
+++ my_test.py
@@ -1,15 +1,19 @@
 import my_debugger
 
 debugger = my_debugger.debugger()
-
+"""
 pid = input("Enter the PID of the process to attach to:")
 debugger.attach(int(pid))
+"""
 
 # 获取wprintf的地址
-printf_address = debugger.func_resolve("C:\\Windows\\System32\\msvcrt.dll", b"printf")
+#printf_address = debugger.func_resolve("./msvcrt.dll", b"GetStdHandle")
+printf_address = debugger.func_resolve("./kernel32.dll", b"wsprintfW")
 print("[*] Address of printf: 0x%08x" % printf_address)
 
+"""
 # 下断点
 debugger.bp_set_sw(printf_address)
 
 debugger.run()
+"""
